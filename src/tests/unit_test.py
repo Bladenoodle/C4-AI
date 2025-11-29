cfb9c9dce831b7c18d38e28b3bede9fc8a9ed633
Erik Vormala <erik.vormala@helsinki.fi>
2025-11-29 15:27:38 +0200

    Add full testing for heuristic functionality and minimax win heuristic return

--- src/tests/unit_test.py
+++ src/tests/unit_test.py
@@ -80,6 +80,70 @@ def test_heuristic_value():
     assert heuristic(board1) > 0
     assert heuristic(board2) < 0
 
+
+def test_heuristic_value():
+    """Checks that heuristic function returns right values"""
+    board1 = create_board()
+    board2 = create_board()
+    make_move(board1, 3, 1)
+    make_move(board2, 3, 2)
+    assert heuristic(board1) > 0
+    assert heuristic(board2) < 0
+
+def test_heuristic_work_as_inteded():
+    """Check that heuristic function calculates as it's intended to.
+    
+    For the test we'll use a random position that is playable for both sides.
+
+    [0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 1, 2, 0, 0]
+    [0, 0, 1, 2, 1, 2, 0]
+    [0, 0, 2, 1, 2, 1, 1]
+
+    The calculation should be calculated with connected diagnals
+    and piece values given by their position in the heuristic value
+    board:
+
+    heuristic_table = [
+        [3, 4, 5, 7, 5, 4, 3],
+        [4, 6, 8, 10, 8, 6, 4],
+        [5, 8, 11, 13, 11, 8, 5],
+        [5, 8, 11, 13, 11, 8, 5],
+        [4, 6, 8, 10, 8, 6, 4],
+        [3, 4, 5, 7, 5, 4, 3],
+    ]
+    Final heuristics for side 1 is the calculated points for side 1
+    minus the calculated points for side 1. This means that the heurstics
+    for side 2 will be the heuristic of side 1 times -1.
+
+    Side 1 points:
+    Pieces = 7 + 13 + 8 + 8 + 4 + 3 = 43
+    Connections = 10 + 2 + 2 + 20 = 34
+    Sum = 77
+
+    Side 2 points:
+    Pieces = 5 + 10 + 11 + 5 + 6 = 37
+    Connections = 10 + 2 + 2 + 2 = 16
+    Sum = 53
+
+    Heuristic for side 1 should be = 77 - 53 = 24
+    Therefore the heuristic for side 2 should be -24.
+    
+    The function should return the heurstic value for side 1
+    which is 24.
+    """
+    board = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0],
+    [0, 0, 1, 2, 1, 2, 0],
+    [0, 0, 2, 1, 2, 1, 1]
+    ]
+    assert heuristic(board) == 24
+
 # Minimax correctness
 
 def test_minimax_win_self():
@@ -87,18 +151,18 @@ def test_minimax_win_self():
     board = create_board()
     for c in range(3):
         make_move(board, c, 1)
-    _, best_move = minimax(board, 3, float('-inf'), float('inf'), True, {}, (2,5))
+    h, best_move = minimax(board, 3, float('-inf'), float('inf'), True, {}, (2,5))
     assert best_move == 3
-
+    assert h == 100002
 
 def test_minimax_win_opponent():
     """Check can minimax spot win in 1 for enemy"""
     board = create_board()
     for c in range(3):
         make_move(board, c, 2)
-    _, best_move = minimax(board, 3, float('-inf'), float('inf'), True, {}, (2,5))
+    h, best_move = minimax(board, 4, float('-inf'), float('inf'), False, {}, (2,5))
     assert best_move == 3
-
+    assert h == -100003
 
 def test_minimax_return_when_depth_0():
     """Check that minimax returns right value at depth 0"""
@@ -120,10 +184,12 @@ def test_minimax_return_when_draw():
     assert (0, None) == minimax(board, 5, float("-inf"), float("inf"), True, {})
 
 def test_depth_calculation():
-    """Testing the minimax with a win in 3 puzzle.
+    """
+    Testing the minimax with a win in 3 puzzle.
        The algorithm should be able to play until win in 3 moves.
        Depth 5 because the turns starts with self,
-       and the win should occur in 3 own moves + 2 opponent moves."""
+       and the win should occur in 3 own moves + 2 opponent moves.
+    """
 
     board = [
     [0, 0, 0, 0, 0, 0, 0],
